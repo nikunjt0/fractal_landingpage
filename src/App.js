2ec5bd4df906522a587d7b4c4b2122ce00d43ff4
arjavchokshi <114848530+arjavchokshi@users.noreply.github.com>
2024-04-06 18:57:15 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//import videoFile from './ASCII - A Mandelbrot Fractal Zoom.mp4'; // Ensure this path is correct
+import logo from './fractallogo.svg'; // Make sure to replace this path with the path to your actual logo
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="menu-bar">
+        <img src={logo} alt="logo" className="logo"/>
+        <nav>
+          <ul>
+            <li><a href="#practice">PRACTICE</a></li>
+            <li><a href="#insights">INSIGHTS</a></li>
+          </ul>
+        </nav>
+      </div>
+      <video className='videoTag' autoPlay loop muted playsInline>
+        <source src={videoFile} type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
